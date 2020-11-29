110eb7e4b81a78208b4b7037cff18ad2851c6af7
Baptiste <baptiste08@live.fr>
2020-11-29 16:02:33 +0100

    Ajout de nouveaux scenarios + 1 action

--- Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/ScenarioResource.java
+++ Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/ScenarioResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import fr.insa.soa.scenarioManagement.model.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 @RestController
 @RequestMapping("/scenario")
@@ -26,4 +27,87 @@ public class ScenarioResource {
 			}
 		}
 	}
+	
+	
+	// SCENARIO entre 7h et 20h TEMP > 20 °C => éteindre les radiateurs d'une pièce 
+	public void turnoffHeating(Classroom classroom) {
+		Sensor t = classroom.getTemp();
+		ArrayList<Actuator> h_list= classroom.getHeating();
+		if (t.getValue()>20){
+			for (int i=0; i< h_list.size();i++){ 
+				h_list.get(i).postDataOM2M(0);
+			}
+		}
+	}
+	
+	// SCENARIO entre 7h et 20h 18°C<TEMP<35°C => ouvrture des fenetres
+	public void openWindows(Classroom classroom) {
+		Sensor t = classroom.getTemp();
+		ArrayList<Actuator> w_list= classroom.getWindows();
+		if (t.getValue()<35 && t.getValue()>18){
+			for (int i=0; i< w_list.size();i++){ 
+				w_list.get(i).postDataOM2M(1);
+			}
+		}
+	}
+	
+
+
+	// SCENARIO entre 18h et 9h Présence --> Allumer les lumières
+	public void PresenceDetection(Classroom classroom) {
+		Sensor p; 
+		ArrayList<Actuator> l_list= classroom.getLights();
+		while(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)>18 && Calendar.getInstance().get(Calendar.HOUR_OF_DAY)<9){
+			p = classroom.getPresence();
+			if (p.getValue()==1){
+				for (int i=0; i< l_list.size();i++){ 
+					l_list.get(i).postDataOM2M(1);
+				}
+			}else{
+				for (int i=0; i< l_list.size();i++){ 
+					l_list.get(i).postDataOM2M(0);
+				}
+			}
+		}
+	}
+	
+	//SCENARIO entre 22h et 6h Présence --> Allumer l'alarme
+	public void setAlarmifPresence(Classroom classroom){
+		Actuator alarme = new Actuator("http://localhost:8080/in-cse/Alarme/DATA","Alarme",0);
+		// ici j'ai définis l'alarme je sais pas si c'était nécessaire;
+		Sensor p;
+		while(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)>22 && Calendar.getInstance().get(Calendar.HOUR_OF_DAY)<6){
+			p = classroom.getPresence();
+			if (p.getValue()==1){
+				alarme.postDataOM2M(1);
+			}
+		}
+	}
+	
+	
+	// ACTION Jours non travail et hors horaires travail + Non présence --> Eteindre Chauffage, Lumières,
+	// Portes et Fenetres
+	public void TurnOffEverything(Classroom classroom){
+		Sensor p = classroom.getPresence();
+		ArrayList<Actuator> l_list= classroom.getLights();
+		ArrayList<Actuator> w_list= classroom.getWindows();
+		ArrayList<Actuator> d_list= classroom.getDoors();
+		ArrayList<Actuator> h_list= classroom.getHeating();
+		if ((!DayHour.isItWorkHours() || DayHour.isItWorkDay()) && p.getValue()==0){
+			for (int i=0; i< l_list.size();i++){ 
+				l_list.get(i).postDataOM2M(0);
+			}
+			for (int i=0; i< w_list.size();i++){ 
+				w_list.get(i).postDataOM2M(0);
+			}
+			for (int i=0; i< d_list.size();i++){ 
+				d_list.get(i).postDataOM2M(0);
+			}
+			for (int i=0; i< h_list.size();i++){ 
+				h_list.get(i).postDataOM2M(0);
+			}
+		}	
+	}
+	
+	
 }
