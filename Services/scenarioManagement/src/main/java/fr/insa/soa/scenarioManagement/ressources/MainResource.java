d2044c09e452bd6db983d798053ff46b917afb31
LorinePose <Lorine13@outlook.es>
2020-12-18 13:03:19 +0100

    correction controller+scenarios

--- Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/MainResource.java
+++ Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/MainResource.java
@@ -1,13 +1,10 @@
 package fr.insa.soa.scenarioManagement.ressources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import fr.insa.soa.scenarioManagement.model.*;
@@ -24,33 +21,38 @@ public class MainResource {
 		while (true){
 			try {
 				ArrayList<Classroom> lc=restTemplate.getForObject(urlClassrooms, ListClassroom.class).getListCl();
+				for (int i=0; i<15;i++){
+					for (Classroom c:lc) {
+						if (DayHour.isItWorkDay() && DayHour.isItWorkHours()){
+							sr.setHeating(c); 
+							System.out.println("Heating handled");
+						
+							sr.openWindows(c);
+							System.out.println("Windows handled");
+						}
+						
+						if(DayHour.isItWorkDay() && DayHour.isItDark()){
+							sr.PresenceDetectionON(c);
+							System.out.println("Presence turn lights ON handled");
+						}
+						
+						if(DayHour.isItForbidden() || !DayHour.isItWorkDay()){
+							sr.setAlarmIfPresence(c);
+							System.out.println("Alarm handled");
+						}
+					}
+				TimeUnit.SECONDS.sleep(20);
+				}
 				for (Classroom c:lc) {
-					
-					if (DayHour.isItWorkDay() && DayHour.isItWorkHours()){
-						sr.setHeating(c); 
-						System.out.println("hakunamatata");
-					
-						sr.openWindows(c);
-						System.out.println("windows handled");
-					}
-					
-					if(DayHour.isItWorkDay() && DayHour.isItDark()){
-						sr.PresenceDetection(c);
-						System.out.println("presence in the dark handled");
-					}
-					
-					if(DayHour.isItForbidden() || !DayHour.isItWorkDay()){
-						sr.setAlarmIfPresence(c);
-						System.out.println("Alarm handled");
-					}
-					
 					if(!DayHour.isItWorkDay() || !DayHour.isItWorkHours()){
 						sr.TurnOffEverything(c);
+						System.out.println("Turning everything off handled");
 					}
-					
+					if(DayHour.isItWorkDay() && DayHour.isItDark()){
+						sr.PresenceDetectionOFF(c);
+						System.out.println("Presence turn lights OFF handled");
+					}
 				}
-				TimeUnit.SECONDS.sleep(20);
-				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/ScenarioResource.java
+++ Services/scenarioManagement/src/main/java/fr/insa/soa/scenarioManagement/ressources/ScenarioResource.java
@@ -1,131 +1,130 @@
 package fr.insa.soa.scenarioManagement.ressources;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import fr.insa.soa.scenarioManagement.model.*;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 @RestController
 @RequestMapping("/scenario")
 public class ScenarioResource {
 	
-	// SCENARIO entre 7h et 20h TEMP < 15°C => allumer les radiateurs d'une pièce 
-	//Implémentation des horaires de fonctionnement de chaque scénario dans le service global 
-	//Récupere une les infos d'une salle et si la temp<15, allume les chauffages avec un POST de 1
+	// SCENARIO TEMP < 15°C => allumer les radiateurs d'une pièce 
+	//Implémentation des horaires de fonctionnement de chaque scénario dans le service global
 	
-	@GetMapping("/heatingON")
+	//Récupere une les infos d'une salle et si la temp<15, allume les chauffages avec un POST de 1
+	@GetMapping("/heating")
 	public int setHeating(Classroom classroom) {
 		Sensor t = classroom.getTemp();
 		ArrayList<Actuator> h_list= classroom.getHeating();
-		if (t.getValue()<15 && h_list.get(0).getValue()!=1){
-			for (int i=0; i< h_list.size();i++){ 
-				h_list.get(i).postDataOM2M(1);
+		if (t.getValue()<15){
+			for (int i=0;i<h_list.size();i++){
+				if(h_list.get(i).getValue()!=1){
+					h_list.get(i).postDataOM2M(1);
+				}
 			}
-		}else if (t.getValue()>20 && h_list.get(0).getValue()!=0){
-			for (int i=0; i< h_list.size();i++){ 
-				h_list.get(i).postDataOM2M(0);
+		}else if (t.getValue()>20){
+			for (int j=0;j<h_list.size();j++){
+				if(h_list.get(j).getValue()!=0){ 
+					h_list.get(j).postDataOM2M(0);
+				}
 			}
 		}
 		return 0;
 	}
-	
-/*	
-	// SCENARIO entre 7h et 20h TEMP > 20 °C => éteindre les radiateurs d'une pièce 
-	@GetMapping("/heatingOFF")
-	public static void turnoffHeating(Classroom classroom) {
-		Sensor t = classroom.getTemp();
-		ArrayList<Actuator> h_list= classroom.getHeating();
-		if (t.getValue()>20){
-			for (int i=0; i< h_list.size();i++){ 
-				h_list.get(i).postDataOM2M(0);
-			}
-		}
-	}
-*/	
-	// SCENARIO entre 7h et 20h 18°C<TEMP<35°C => ouvrture des fenetres
-	@GetMapping("/windowsON")
+
+	// SCENARIO 18°C<TEMP<35°C => ouvrture des fenetres
+	@GetMapping("/windows")
 	public void openWindows(Classroom classroom) {
 		Sensor t = classroom.getTemp();
 		ArrayList<Actuator> w_list= classroom.getWindows();
-		if (t.getValue()<35 && t.getValue()>18 && w_list.get(0).getValue()!=1){
-			for (int i=0; i< w_list.size();i++){ 
-				w_list.get(i).postDataOM2M(1);
+		if (t.getValue()<30 && t.getValue()>18){
+			for (int i=0;i<w_list.size();i++){
+				if(w_list.get(i).getValue()!=1){
+					w_list.get(i).postDataOM2M(1);
+				}
+			}
+		}else if (t.getValue()<15 || t.getValue()>35){
+			for (int i=0;i<w_list.size();i++){
+				if(w_list.get(i).getValue()!=0){
+					w_list.get(i).postDataOM2M(0);
+				}
 			}
 		}
 	}
 	
 
-
-	// SCENARIO entre 18h et 9h Présence --> Allumer les lumières
-	@GetMapping("/presence")
-	public void PresenceDetection(Classroom classroom) {
+	// SCENARIO Présence --> Allumer les lumières
+	@GetMapping("/presenceON")
+	public void PresenceDetectionON(Classroom classroom) {
 		Sensor p; 
 		ArrayList<Actuator> l_list= classroom.getLights();
-		//while(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)>18 && Calendar.getInstance().get(Calendar.HOUR_OF_DAY)<9){
 		p = classroom.getPresence();
-		if (p.getValue()==1 && l_list.get(0).getValue()!=1){
-			for (int i=0; i< l_list.size();i++){ 
-				l_list.get(i).postDataOM2M(1);
+		if (p.getValue()==1){
+			for (int i=0;i<l_list.size();i++){
+				if(l_list.get(i).getValue()!=1){
+					l_list.get(i).postDataOM2M(1);
+				}
 			}
-		}else if(p.getValue()==0 && l_list.get(0).getValue()!=0){
-			for (int i=0; i< l_list.size();i++){ 
+		}	
+	}
+	@GetMapping("/presenceOFF")
+	public void PresenceDetectionOFF(Classroom classroom) {
+		Sensor p; 
+		ArrayList<Actuator> l_list= classroom.getLights();
+		p = classroom.getPresence();
+		if (p.getValue()==0){
+			for (int i=0;i<l_list.size();i++){
+				if(l_list.get(i).getValue()!=0){
 					l_list.get(i).postDataOM2M(0);
+				}
 			}
 		}
-		
 	}
 	
-	//SCENARIO entre 22h et 6h Présence --> Allumer l'alarme
+	//SCENARIO Présence --> Allumer l'alarme
 	@GetMapping("/alarm")
 	public void setAlarmIfPresence(Classroom classroom){
 		Actuator alarm = new Actuator("http://localhost:8080/in-cse/Alarme/DATA","Alarme",0);
-		// ici j'ai définis l'alarme je sais pas si c'était nécessaire;
-		Sensor p;
-		//while(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)>22 && Calendar.getInstance().get(Calendar.HOUR_OF_DAY)<6){
-		p = classroom.getPresence();
+		Sensor p = classroom.getPresence();
 		if (p.getValue()==1 && alarm.getValue()!=1){
 			alarm.postDataOM2M(1);
 		}
-		//}
 	}
 	
 	
-	// ACTION Jours non travail et hors horaires travail + Non présence --> Eteindre Chauffage, Lumières,
-	// Portes et Fenetres
+	// Non présence --> Eteindre Chauffage, Lumières, Portes et Fenetres
 	@GetMapping("/OFF")
 	public void TurnOffEverything(Classroom classroom){
-		//Sensor p = classroom.getPresence();
+		Sensor p = classroom.getPresence();
 		ArrayList<Actuator> l_list= classroom.getLights();
 		ArrayList<Actuator> w_list= classroom.getWindows();
 		ArrayList<Actuator> d_list= classroom.getDoors();
 		ArrayList<Actuator> h_list= classroom.getHeating();
-		//if ((!DayHour.isItWorkHours() || DayHour.isItWorkDay()) && p.getValue()==0){
-		if(l_list.get(0).getValue()!=0){	
-			for (int i=0; i< l_list.size();i++){ 
+		if (p.getValue()==0){
+			for (int i=0;i<l_list.size();i++){
+				if(l_list.get(i).getValue()!=0){	
 					l_list.get(i).postDataOM2M(0);
 				}
-		}
-		if(w_list.get(0).getValue()!=0){	
-			for (int i=0; i< w_list.size();i++){ 
-				w_list.get(i).postDataOM2M(0);
+			}
+			for (int i=0;i<w_list.size();i++){
+				if(w_list.get(i).getValue()!=0){	
+					w_list.get(i).postDataOM2M(0);
+				}
+			}
+			for (int i=0;i<d_list.size();i++){
+				if(d_list.get(i).getValue()!=0){
+					d_list.get(i).postDataOM2M(0);
+				}
+			}
+			for (int i=0;i<h_list.size();i++){
+				if(h_list.get(i).getValue()!=0){
+					h_list.get(i).postDataOM2M(0);
+				}
 			}
 		}
-		if(d_list.get(0).getValue()!=0){
-			for (int i=0; i< d_list.size();i++){ 
-				d_list.get(i).postDataOM2M(0);
-			}
-		}
-		if(h_list.get(0).getValue()!=0){
-			for (int i=0; i< h_list.size();i++){ 
-				h_list.get(i).postDataOM2M(0);
-			}
-		}	
 	}
 }
